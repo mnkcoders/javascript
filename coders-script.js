525d9ec16dc8d4eac9bac70efc4bef68b8394fc6
unknown <jaume.llopis1@gmail.com>
2019-10-08 11:08:05 +0200

    html builder

--- coders-script.js
+++ coders-script.js
@@ -1,3 +1,83 @@
+function CoderTools() {
+
+    
+}
+
+
+CoderTools.Geolocation = function () {
+
+    return this;
+};
+
+CoderTools.Geolocation.Current = function(){
+    
+    window.navigator.geolocation.getCurrentPosition(position => {
+        console.log(position);
+    }, error => {
+        console.error(error);
+    });
+};
+
+
+CoderTools.HTML = function( element, attributes , content ){
+    
+    var _node = {
+        'name': typeof element !== 'undefined' ? element : 'div',
+        'attributes': [],
+        'content' : typeof content !== 'undefined' ? content : false
+    };
+    /**
+     * @returns {String}
+     */
+    this.toString = function(){
+
+        return ( _node.content !== false ) ?
+            '<' + _node.name + ' ' + this.serializeAtts() + '>' + content.toString() + '</' + _node.name + '>' :
+            '<' + _node.name + ' ' + this.serializeAtts() + '/>';
+    };
+    /**
+     * 
+     * @param {type} atts
+     * @returns {Array}
+     */
+    this.importAtts = function( atts ){
+        
+        switch( typeof atts ){
+            case 'string':
+                break;
+            case 'object':
+                for( var key in atts ){
+                    _node.attributes[ key ] = Array.isArray( atts[ key ] ) ?
+                            atts[ key ].join(' ') :
+                            atts[ key ].toString();
+                }
+                break;
+            case 'number':
+                break;
+        }
+        
+        return this;
+    };
+    /**
+     * @returns {String}
+     */
+    this.serializeAtts = function( ){
+      
+        var atts = [];
+
+        for( var key in _node.attributes ){
+            
+            var val = _node.attributes[ key ];
+            
+            atts.push( key + '="' + val + '"' );
+        }
+        
+        return atts.join(' ');
+    };
+    
+    return this.importAtts( attributes );
+};
+
 /**
  * @type CookieManager
  */
@@ -150,11 +230,3 @@ class CookieManager {
         return output.join('; ');
     }
 }
-
-
-
-            window.navigator.geolocation.getCurrentPosition( position => {
-                console.log( position );
-            }, error => {
-                console.error( error );
-            });
